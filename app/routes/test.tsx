133da3fbd56845fd05317929a9557b08551b176c
joshigx <49166230+joshigx@users.noreply.github.com>
2025-12-21 21:55:29 +0100

    test db

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,3 +1,21 @@
+import type { Route } from "./+types/test.ts";
+import { Pool } from "npm:pg";
+
+// Diese Funktion läuft server-side
+export async function loader({ params }: Route.LoaderArgs) {
+
+
+  const pool = new Pool();
+
+  const _createTable = await pool.query('CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL)');
+
+
+
+  
+  return ("");
+}
+
+
 export default function Home() {
   return (
   <>
