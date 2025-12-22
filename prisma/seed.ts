c35935b8d2ab4e4ed9abda005fca49eb679c16cd
Josua Lucas <josua.lucas00@gmail.com>
2025-12-22 16:28:44 +0100

    kleinigkeiten

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,12 @@
+//seed with: npx prisma db seed
+
+
 import { PrismaClient, Prisma } from "../app/generated/prisma/client.ts";
-import "dotenv/config"; 
-import { PrismaPg } from "@prisma/adapter-pg";
-import {  env } from "prisma/config";
+import { PrismaPg } from "@prisma/adapter-pg";;
+import process from "node:process";
 
 const adapter = new PrismaPg({
-  connectionString: "prisma+postgres://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqd3RfaWQiOjEsInNlY3VyZV9rZXkiOiJza19OVDRDa2JKcEJXUkFPQkpHbWY3ajciLCJhcGlfa2V5IjoiMDFLRDM0NVFLUDYwWVo0OVE0UzlKQVdLTjciLCJ0ZW5hbnRfaWQiOiI1ZjI0OGQyNDMzMGY2YjliY2MyZmYyOWFhNmVjOTkzMzA1NGY5ZTA3ZTJjZDFiMjcwNzI3MzBhZjUxNjQ5YzFhIiwiaW50ZXJuYWxfc2VjcmV0IjoiNzllYzNmOWMtYzUxYy00MDY0LTk2MTAtYzJmYmRlNzE1ZDE4In0.KBRnJkWsXWMaM7j2-TPjmC77Hm7GQXxbXpCL1D0Ky20",
+  connectionString: process.env.DATABASE_URL!,
 });
 
 const prisma = new PrismaClient({
@@ -13,35 +15,23 @@ const prisma = new PrismaClient({
 
 const userData: Prisma.UserCreateInput[] = [
   {
-    name: "Alice",
-    email: "alice@prisma.io",
+    name: "Seedwennsgehtistgeil",
+    email: "test@test.test",
     posts: {
       create: [
         {
-          title: "Join the Prisma Discord",
-          content: "https://pris.ly/discord",
+          title: "Join tasdfrd",
+          content: "https:/discord",
           published: true,
         },
         {
-          title: "Prisma on YouTube",
-          content: "https://pris.ly/youtube",
+          title: "PrismauTube",
+          content: "httpis.ly/youtube",
         },
       ],
     },
   },
-  {
-    name: "Bob",
-    email: "bob@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Follow Prisma on Twitter",
-          content: "https://www.twitter.com/prisma",
-          published: true,
-        },
-      ],
-    },
-  },
+
 ];
 
 export async function main() {
