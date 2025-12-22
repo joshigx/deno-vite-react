5ed2baed0f97ca85b929d79ae5604e5c8d756a8e
joshigx <49166230+joshigx@users.noreply.github.com>
2025-12-22 16:17:10 +0100

    jetzt gehts irgendwie mit datenbank zugriff

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
-import type { Route } from "./+types/home.ts";
-import { Welcome } from "../welcome/welcome.tsx";
+import type { Route } from "../../.react-router/types/app/routes/+types/home.ts";
+import prisma from '~/lib/prisma'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,17 +8,25 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export function loader() {
-  return {
-    denoVersion: Deno.version.deno,
-  };
+export async function loader() {
+  const users = await prisma.user.findMany();
+  return { users };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
+   const { users } = loaderData;
   return (
-  <>
-  <Welcome denoVersion={loaderData.denoVersion} />
-  <p>Test</p>
-</>
+    <div className="min-h-screen flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+         {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
