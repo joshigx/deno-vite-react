1c4074295712b2a14ead6224ddf2aa3ef1c200f8
joshigx <joshi.gx@gmail.com>
2025-12-23 10:18:41 +0100

    typen hinzugefügt

--- app/lib/prisma.server.ts
+++ app/lib/prisma.server.ts
@@ -1,14 +1,15 @@
 // app/lib/prisma.server.ts
-import 'dotenv/config';
+import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 import process from "node:process";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({connectionString});
+const connectionString: string = `${process.env.DATABASE_URL}`;
+const adapter: PrismaPg = new PrismaPg({ connectionString });
+const adapterObject = {adapter: adapter};
 
 export function createPrismaClient() {
-  return new PrismaClient({
-    adapter,
-  });
+  return (
+    new PrismaClient(adapterObject)
+  );
 }
