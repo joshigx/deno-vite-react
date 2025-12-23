5dcaa756072e1fd4a1b3bd56eba251972f41780d
joshigx <joshi.gx@gmail.com>
2025-12-23 09:37:00 +0100

    refactores prisma.server.ts

--- app/lib/prisma.server.ts
+++ app/lib/prisma.server.ts
@@ -1,11 +1,11 @@
 // app/lib/prisma.server.ts
+import 'dotenv/config';
 import { PrismaClient } from "../generated/prisma/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
 import process from "node:process";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({connectionString});
 
 export function createPrismaClient() {
   return new PrismaClient({
